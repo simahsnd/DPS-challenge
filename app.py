2fd77cca9da4eb51d544d1afda9026b375569a49
sima-git <simahsnd@gmail.com>
2022-10-27 01:51:57 +0200

    Third Commit

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def predictor():
     return render_template('pipeline_predictor.html', predicted_value=predicted_value)
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=5000)
